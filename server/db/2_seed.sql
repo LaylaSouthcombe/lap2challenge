ccc9084c6ecb5d580ec216af426a9770a8fc2c51
Shoreen Beckford <shoreen.beckford@gmail.com>
2022-05-30 17:24:33 +0100

    Amend urlEnd to url_end and change last comma to a ';'

--- server/db/2_seed.sql
+++ server/db/2_seed.sql
@@ -1,6 +1,6 @@
-INSERT INTO articles (title, name, body, urlEnd)
+INSERT INTO articles (title, name, body, url_end)
 VALUES
     ('Test Title', 'Test Name', 'Test Body', '123'),
     ('Title 2', 'Pseudonym', 'Body 2', '456'),
-    ('Title Last', 'Final Name', 'Last Body', '789'),
+    ('Title Last', 'Final Name', 'Last Body', '789');
     
