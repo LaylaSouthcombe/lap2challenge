f8a56c842fe61edaee2f6b3fa2b5fcedfb054992
Shoreen Beckford <shoreen.beckford@gmail.com>
2022-05-30 18:04:41 +0100

    Add routes to /controllers/post.js

---
+++ server/api/controllers/posts.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const Article = require('../models/post');
+
+// articles show route
+router.get('/:url_end', async (req, res) => {
+    try {
+        const article = await Article.findById(parseInt(req.params.url_end))
+        res.json(article)
+    } catch(err) {
+        res.status(404).json({err})
+    }
+})
+
+// Create article route
+router.post('/', async (req, res) => {
+    try {
+        const article = await Article.create(req.body.title, req.body.name, req.body.body, req.body.url_end )
+        res.json(article)
+    } catch(err) {
+        res.status(404).json({err})
+    }
+})
