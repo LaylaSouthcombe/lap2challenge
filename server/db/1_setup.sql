33795f63196f26f8afd8d129eb0dac230a67bd8e
Shoreen Beckford <shoreen.beckford@gmail.com>
2022-05-30 17:48:48 +0100

    Add feedback branch

--- server/db/1_setup.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS articles;
-
-CREATE TABLE articles (
-    id serial PRIMARY KEY,
-    title varchar(50) NOT NULL,
-    name varchar(255) NOT NULL,
-    body varchar(255) NOT NULL,
-    url_end varchar NOT NULL
-);
--- server/db/2_seed.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO articles (title, name, body, url_end)
-VALUES
-    ('Test Title', 'Test Name', 'Test Body', '123'),
-    ('Title 2', 'Pseudonym', 'Body 2', '456'),
-    ('Title Last', 'Final Name', 'Last Body', '789');
-    
